4f6e0dfe1762ee9f4071b4bdd7a292c9aa92bea2
Brian Guido Grieco <brian.g.grieco@gmail.com>
2024-03-05 10:35:30 +0100

    Added SVGR webpack config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-module.exports = {
-    webpack(config) {
-        config.module.rules.push({
-            test: /\.svg$/,
-            use: ['@svgr/webpack'],
-        });
-        return config;
-    },
-};
-
-export default nextConfig;
+export function webpack(config) {
+    config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    });
+    return config;
+}
