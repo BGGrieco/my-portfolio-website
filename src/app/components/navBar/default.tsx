423e937c149c2013f5724a47f34abd092db4bc4c
Brian Guido Grieco <brian.g.grieco@gmail.com>
2024-03-15 14:27:22 +0100

    Implemented menu indicator, working on menu show/hide

--- src/app/components/navBar/default.tsx
+++ src/app/components/navBar/default.tsx
@@ -1,8 +1,66 @@
 "use client";
+import React, { useState, useLayoutEffect, useEffect } from "react";
+import Image from "next/image";
 import "./default.scss";
-import Selector from "../../../../public/selector.svg";
+
+const MenuHighlight = () => (
+  <Image
+    src="/selector.png"
+    height={101}
+    width={480}
+    className="selectorImage"
+    alt="Menu highlight"
+  />
+);
+
+// Restrict value to be between the range [0, value]
+const clamp = (value: number) => Math.max(0, value);
+
+// Check if number is between two values
+const isBetween = (value: number, floor: number, ceil: number) =>
+  value >= floor && value <= ceil;
+
+const useScrollspy = (ids: string[], offset: number = 0) => {
+  const [activeId, setActiveId] = useState("");
+
+  useLayoutEffect(() => {
+    const listener = () => {
+      const scroll = window.scrollY;
+
+      const position = ids
+        .map((id) => {
+          const element = document.getElementById(id);
+
+          if (!element) return { id, top: -1, bottom: -1 };
+
+          const rect = element.getBoundingClientRect();
+          const top = clamp(rect.top + scroll - offset);
+          const bottom = clamp(rect.bottom + scroll - offset);
+
+          return { id, top, bottom };
+        })
+        .find(({ top, bottom }) => isBetween(scroll, top, bottom));
+
+      setActiveId(position?.id || "");
+    };
+
+    listener();
+
+    window.addEventListener("resize", listener);
+    window.addEventListener("scroll", listener);
+
+    return () => {
+      window.removeEventListener("resize", listener);
+      window.removeEventListener("scroll", listener);
+    };
+  }, [ids, offset]);
+
+  return activeId;
+};
 
 export default function NavBar() {
+  const ids = ["projectsSection", "experienceSection", "skillSection", "aboutSection"];
+  const activeId = useScrollspy(ids, 350);
   return (
     <div className="navBar">
       <div className="card">
@@ -17,21 +75,21 @@ export default function NavBar() {
             });
           }}
         >
-          <li>
+          <li className={activeId == "projectsSection" ? "active" : ""}>
+            <MenuHighlight />
             <a href="#projects">Projects</a>
-            <Selector />
           </li>
-          <li>
+          <li className={activeId == "experienceSection" ? "active" : ""}>
+            <MenuHighlight />
             <a href="#experience">Experience</a>
-            <Selector />
           </li>
-          <li>
+          <li className={activeId == "skillSection" ? "active" : ""}>
+            <MenuHighlight />
             <a href="#skills">Skills</a>
-            <Selector />
           </li>
-          <li>
+          <li className={activeId == "aboutSection" ? "active" : ""}>
+            <MenuHighlight />
             <a href="#about">About</a>
-            <Selector />
           </li>
         </ul>
       </div>
