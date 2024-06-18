9e9ba96b798520eab81ae0c48a8ca929cf0b4b0f
Brian Guido Grieco <brian.g.grieco@gmail.com>
2024-06-18 11:49:39 +0200

    Preparing for v1

--- src/app/components/welcomeCard/default.tsx
+++ src/app/components/welcomeCard/default.tsx
@@ -7,12 +7,9 @@ export default function WelcomeCard() {
   return (
     <div className="card relativelyCentered centeredText fullWidth">
       <h1>Brian G. Grieco</h1>
-      <h2 className="dark marginBottom_sm">
+      <h2 className="dark marginBottom_lrg">
         UX Research | UI Design | Front-End Dev
       </h2>
-      <h3 className="marginBottom_med">
-        Location: <span>Milan, Italy</span>
-      </h3>
       <div className="buttonContainer">
         <Button href="mailto:brian.g.grieco@gmail.com" svg={<EmailIcon />} text="brian.g.grieco@gmail.com" />
         <Button href="tel:+393707026255" svg={<PhoneIcon />} text="+39 370 702 6255" />
