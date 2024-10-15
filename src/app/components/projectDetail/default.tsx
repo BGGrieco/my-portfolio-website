dc8c6959f3b0f5f42e774671b6ba781c85387cc3
Brian Guido Grieco <brian.g.grieco@gmail.com>
2024-10-15 18:18:25 +0200

    implemented project detail cards TODO make responsive and update images

--- src/app/components/projectDetail/default.tsx
+++ src/app/components/projectDetail/default.tsx
@@ -1,6 +1,6 @@
 import "./default.scss";
 
-export default function ProjectDetail({ className, title, subTitle, imageUrl, blurb }) {
+export default function ProjectDetail({ className, title, subTitle, imageUrl, entities, blurb1, blurb2, blurb3, blurb4, role }) {
   return (
     <div className={className}>
       <img className="detailImage" src={imageUrl} />
@@ -8,7 +8,17 @@ export default function ProjectDetail({ className, title, subTitle, imageUrl, bl
         <div className="closeButton"></div>
         <h2>{title}</h2>
         <h3>{subTitle}</h3>
-        <p className="marginTop_sm">{blurb}</p>
+        <p className="marginTop_sm"><b>Involved Entities:</b></p>
+        <p>{entities}</p>
+        <p className="marginTop_xsm"><b>Project Requirements:</b></p>
+        <ul>
+          <li>{blurb1}</li>
+          <li>{blurb2}</li>
+          <li>{blurb3}</li>
+          <li>{blurb4}</li>
+        </ul>
+        <p className="marginTop_xsm"><b>Role and Responsibilities:</b></p>
+        <p>{role}</p>
       </div>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,14 +9,41 @@ import ProjectCard from "./components/projectCard/default";
 import ExperienceCard from "./components/experienceCard/default";
 import SkillCard from "./components/skillCard/default";
 import AboutCard from "./components/aboutCard/default";
-import ExperienceInfo from "./experience.json";
 import ProjectDetail from "./components/projectDetail/default";
+import ProjecstInfo from "./projects.json";
+import ExperienceInfo from "./experience.json";
+import { NEXT_PROJECT_ROOT_DIST } from "next/dist/build/webpack-config";
 
 export default function Home() {
 
+  const [projectContent, setProjectContent] = useState(ProjecstInfo.cyberPilot);
   const [open, setOpen] = useState(false);
 
-  const handleProjectClick = event => {
+  const handleProjectClick = (event) => {
+    const value = event.currentTarget.getAttribute("id");
+    
+    switch (value) {
+      case "firstCard": {
+        setProjectContent(ProjecstInfo.cyberPilot);
+      }
+      break;
+      case "secondCard": {
+        setProjectContent(ProjecstInfo.brembo);
+      }
+      break;
+      case "thirdCard": {
+        setProjectContent(ProjecstInfo.areaClienti);
+      }
+      break;
+      case "fourthCard": {
+        setProjectContent(ProjecstInfo.unifarco);
+      }
+      break;
+      default: {
+        console.log("No Card");
+      }
+    }
+
     setOpen(current => !current);
   };
 
@@ -37,10 +64,15 @@ export default function Home() {
 
         <ProjectDetail
           className={`card projectDetail ${open ? 'open' : ''}`}
-          title="Windtre Business"
-          subTitle="Cyber Security Portal"
-          imageUrl="/cyberPilotDashboard.png"
-          blurb="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+          imageUrl={projectContent.imageURL}
+          title={projectContent.title}
+          subTitle={projectContent.subtitle}
+          entities={projectContent.entities}
+          blurb1={projectContent.blurb1}
+          blurb2={projectContent.blurb2}
+          blurb3={projectContent.blurb3}
+          blurb4={projectContent.blurb4}
+          role={projectContent.role}
         />
 
         <div id="projectsSection" className="section">
@@ -56,28 +88,28 @@ export default function Home() {
           />
 
           <div className="squaresGrid">
-            <div className="projectCardContainer" onClick={handleProjectClick}>
+            <div id="firstCard" className="projectCardContainer" onClick={handleProjectClick}>
               <ProjectCard
                 title="Wind3 Business"
                 subTitle="Cyber Security Portal"
                 imageUrl="/w3bCTI.png"
               />
             </div>
-            <div className="projectCardContainer" onClick={handleProjectClick}>
+            <div id="secondCard" className="projectCardContainer" onClick={handleProjectClick}>
               <ProjectCard
                 title="Custom Fit"
                 subTitle="The Beginning"
                 imageUrl="/customFit.png"
               />
             </div>
-            <div className="projectCardContainer" onClick={handleProjectClick}>
+            <div id="thirdCard" className="projectCardContainer" onClick={handleProjectClick}>
               <ProjectCard
                 title="patientMpower"
                 subTitle="Patient app"
                 imageUrl="/pMpApp.png"
               />
             </div>
-            <div className="projectCardContainer" onClick={handleProjectClick}>
+            <div id="fourthCard" className="projectCardContainer" onClick={handleProjectClick}>
               <ProjectCard
                 title="The Irish Times"
                 subTitle="Listen"
