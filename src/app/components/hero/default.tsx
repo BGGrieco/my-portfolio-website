d1b3807c8d9333d9706ea1ca40840d4778e90a90
Brian Guido Grieco <brian.g.grieco@gmail.com>
2025-09-22 19:36:57 +0200

    Header fixed

--- src/app/components/hero/default.tsx
+++ src/app/components/hero/default.tsx
@@ -1,22 +1,8 @@
-"use client";
-import React, { useState, useEffect, useRef } from "react";
-import Image from "next/image";
 import "./default.scss";
 
 export default function Hero() {
-  const headerImageRef = useRef(null);
 
   return (
-    <div className="hero">
-      <Image
-        src="/duomo.jpg"
-        alt="Hero Image of the duomo in Milan"
-        className="heroImage"
-        width={1620}
-        height={1080}
-        priority={true}
-        ref={headerImageRef}
-      />
-    </div>
+    <div className="hero"></div>
   );
 }
